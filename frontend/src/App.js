80d0ad3028c935df53d00f91f9c2eaeae37154c3
Peter Maged <petermaged222@gmail.com>
2023-01-11 04:51:41 +0200

    frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment >
+
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import store from "./store";
+import { Provider } from "react-redux";
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
