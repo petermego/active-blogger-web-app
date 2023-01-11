18fd0c861751ff11d8dd31c95d75b1b9b46d3c46
Peter Maged <petermaged222@gmail.com>
2023-01-11 05:03:16 +0200

    ff

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Origin", "GET, POST, PUT, DELETE");
@@ -12,7 +13,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
